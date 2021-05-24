a293fbd512facd278d01a1939f1d5c77893d662b
남궁성훈 <stomer1@naver.com>
2021-05-24 14:09:14 +0900

    글로벌 scope 내장 scope

--- adv_function_scope.py
+++ adv_function_scope.py
@@ -11,3 +11,20 @@ def scope_func2(a):
 print(scope_func(10))
 print(scope_func2(10))
 print("global x:", x) # scope_func2에서 x가 치환됬지만 global x는 변경되지 않음
+
+g = 1 # 글로벌 변수
+
+def scope_func3(a):
+    # 훔수 내부에서 전역 객체를 사용해야 한다 -> global 키워드를 사용
+    global g
+    g = 3
+    return a + g
+
+print(scope_func3(10))
+print("global g:", g)
+
+# 글로벌 영역의 확인
+print(dir()) # globals
+# 내장 영역의 확인
+print("내장영역:", dir('__builtins__'))
+
